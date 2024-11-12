439ff67f65635786c194d5ea4c188cd358396879
serafima8 <antonsaltikov@gmail.com>
2024-11-12 17:15:34 +0200

    add

---
+++ WebApplication2/wwwroot/js/shared_script.js
@@ -0,0 +1,12 @@
+﻿
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        let header = document.getElementById('header-top');
+        let scrollTop = window.scrollY;
+        let maxScroll = 250;
+
+        let opacity = Math.min(scrollTop / maxScroll, 1);
+        header.style.backgroundColor = `rgba(115, 152, 172, ${opacity})`;
+    });
+});
+
